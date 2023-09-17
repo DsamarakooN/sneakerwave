07dd0741466372eab3b1bcbcffc15de2e192dbd3
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-09-17 18:20:15 +0530

    component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,42 @@
+// import Hero from './sections/Hero';
+// import PopularProducts from './sections/PopularProducts';
+// import SuperQuality from './sections/SuperQuality';
+// import Services from './sections/Services';
+// import SpecialOffers from './sections/SpecialOffers';
+// import CustomerReviews from './sections/CustomerReviews';
+// import Subscribe from './sections/Subscribe';
+// import Footer from './sections/Footer';
+
+
+import {CustomerReviews , Footer , Hero , PopularProducts , Services , SpecialOffer , Subscribe , SuperQuality} from './sections';
+
+
 const App = () => (
   <main className="relative">
     Nav
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero/>
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts/>
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality/>
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services/>
     </section>
     <section className="padding">
-      SpecialOffers
+      <SpecialOffer/>
     </section>
     <section className="bg-pale-blue padding">
-      CustomerReviews
+      <CustomerReviews/>
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe/>
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      Footer
+      <Footer/>
     </section>
   </main>
 )
